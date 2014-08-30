8dda7630cab99711f8b87ef9f2aa78a3956d19d6
Jesse Gumm <gumm@sigma-star.com>
2014-08-30 16:45:45 -0500

    Add support for lists of tuples

--- src/element_quicktable.erl
+++ src/element_quicktable.erl
@@ -4,17 +4,29 @@
 
 reflect() -> record_info(fields, quicktable).
 
-%% Expects data to be a list of lists. IE
+%% Expects data to be a list of lists, or a list of tuples:
 %%
 %% [
 %% 		[row1_cell1,row1_cell2,row1_cell3],
 %% 		[row2_cell1,row2_cell2,row2_cell3],
 %% 		...
 %% 	].
+%%
+%%  or
+%%
+%% [
+%% 		{row1_cell1,row1_cell2,row1_cell3},
+%% 		{row2_cell1,row2_cell2,row2_cell3},
+%% 		...
+%% 	].
 render_element(Rec = #quicktable{}) ->
 	#table{
 		class=Rec#quicktable.class,
 		rows=lists:map(fun(Row) ->
+			Cells = case is_tuple(Row) of
+				true -> tuple_to_list(Row);
+				false -> Row
+			end,
 			#tablerow{cells=lists:map(fun(Cell) ->
 				case Rec#quicktable.mode of
 					body -> #tablecell{body=Cell};
